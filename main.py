0c294836ac6e1d64846590f137bf7624421a1fce
JeongSuminn <susuhome2@gmail.com>
2024-05-30 02:12:07 +0900

    fastapi 환경 설정

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from todo import todo_router
+import uvicorn
+
+app = FastAPI()
+
+origins = ["http://127.0.0.1:5500", "http://18.207.12.132"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+@app.get("/") #루트 경로 0.0.0.0:8000/어쩌구
+async def welcome() -> dict:
+    return {
+        "msg" : "hello world????"
+    }
+
+app.include_router(todo_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
