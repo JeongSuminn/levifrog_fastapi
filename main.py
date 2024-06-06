6d7b85c399c2cc40ccb282526f6f1969dada2e24
JeongSuminn <susuhome2@gmail.com>
2024-06-07 00:29:16 +0900

    add Dockerfile & .dockerignore modify main.py - port number 8083 to 80

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ async def welcome() -> dict:
 app.include_router(msg_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8083, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
